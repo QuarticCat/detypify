d86b7caf266832669b9ec8f48997c2f36b99e16a
QuarticCat <QuarticCat@pm.me>
2024-03-27 06:10:18 +0800

    impl navbar

--- web/App.svelte
+++ web/App.svelte
@@ -1,5 +1,26 @@
 <script>
-    let name = "world";
+    import "./app.pcss";
+    import { Navbar, NavBrand, ToolbarButton, Tooltip, DarkMode } from "flowbite-svelte";
+    import { GithubSolid } from "flowbite-svelte-icons";
 </script>
 
-<h1>Hello {name}!</h1>
+<Navbar>
+    <NavBrand>
+        <!-- <img src={logo} class="me-3 h-8" alt="Detexify Logo" /> -->
+        <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+            Detypify
+        </span>
+    </NavBrand>
+    <div class="ms-auto flex items-center">
+        <ToolbarButton
+            size="lg"
+            class="inline-block hover:text-gray-900 dark:hover:text-white"
+            href="https://github.com/QuarticCat/detypify"
+        >
+            <GithubSolid size="lg" />
+        </ToolbarButton>
+        <Tooltip class="dark:bg-gray-900" placement="bottom">View on GitHub</Tooltip>
+        <DarkMode size="lg" class="inline-block hover:text-gray-900 dark:hover:text-white" />
+        <Tooltip class="dark:bg-gray-900" placement="bottom">Toggle dark mode</Tooltip>
+    </div>
+</Navbar>
