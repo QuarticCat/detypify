8f42c22405ba35b1ab949e500a6d501085ba2255
QuarticCat <QuarticCat@pm.me>
2024-03-31 00:59:07 +0800

    change website base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('vite').UserConfig} */
 export default {
     root: "web",
-    base: "/detypify",
     build: {
         outDir: "../web-out",
         emptyOutDir: true,
