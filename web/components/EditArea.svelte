e112a6b8d13d5de62accf86a95262b2ccec209d3
QuarticCat <QuarticCat@pm.me>
2024-04-05 07:58:38 +0800

    impl sample submitting

--- web/components/EditArea.svelte
+++ web/components/EditArea.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Button, Input } from "flowbite-svelte";
+    import { Button, Input, Spinner } from "flowbite-svelte";
     import { RefreshOutline } from "flowbite-svelte-icons";
     import { imgUrl, inputText, savedSamples, strokes } from "../store";
     import MyButton from "../utils/Button.svelte";
@@ -15,6 +15,8 @@
     let disableSubmit = true;
 
     let sampleId = 0;
+
+    let isSubmitting = false;
 
     function validateInput(input) {
         if (input === "") {
@@ -50,11 +52,32 @@
         $strokes = [];
     }
 
-    function submit() {}
+    async function submit() {
+        let samples = $savedSamples.map(({ name, strokes }) => [name, strokes]);
+
+        isSubmitting = true;
+        let form = new FormData();
+        form.append("c", JSON.stringify(samples));
+        let response = await fetch(`https://corsproxy.io/?${encodeURIComponent("https://pb.mgt.moe/")}`, {
+            method: "POST",
+            body: form,
+        });
+        let text = await response.text();
+        isSubmitting = false;
+
+        $savedSamples = [];
+
+        let pasteUrl = text.match(/url: (.*?)\n/)[1];
+        window.open(
+            `https://github.com/QuarticCat/detypify-data/issues/new` +
+                `?title=${encodeURIComponent("[Samples] v0.1.0")}` +
+                `&body=${encodeURIComponent(`${pasteUrl}\n\n<!-- Do not modify -->`)}`,
+        );
+    }
 
     $: validateInput($inputText);
     $: disableSave = inputColor !== "green" || $strokes.length === 0;
-    $: disableSubmit = $savedSamples.length === 0;
+    $: disableSubmit = isSubmitting || $savedSamples.length === 0;
 </script>
 
 <div>
@@ -65,5 +88,11 @@
 
 <div class="flex justify-around">
     <Button class="w-5/12" disabled={disableSave} on:click={save}>Save</Button>
-    <Button class="w-5/12" disabled={disableSubmit} on:click={submit}>Submit</Button>
+    <Button class="w-5/12" disabled={disableSubmit} on:click={submit}>
+        {#if isSubmitting}
+            <Spinner size="5" />
+        {:else}
+            Submit
+        {/if}
+    </Button>
 </div>
