008edf926054f586d4de8678d06c2bc0cf98bac2
QuarticCat <QuarticCat@pm.me>
2025-12-24 17:47:04 +0800

    use AdmaW with scheduler

--- python/train.py
+++ python/train.py
@@ -47,8 +47,11 @@ def train_loop(
 
 
 def test_loop(
-    dataloader: DataLoader, model: nn.Module, loss_fn: LossFn, device: torch.device
-):
+    dataloader: DataLoader,
+    model: nn.Module,
+    loss_fn: LossFn,
+    device: torch.device,
+) -> float:
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -64,6 +67,7 @@ def test_loop(
     test_loss /= num_batches
     correct /= size
     print(f"Test > acc: {(100 * correct):>0.1f}%, avg loss: {test_loss:>8f}")
+    return test_loss
 
 
 if __name__ == "__main__":
@@ -71,20 +75,28 @@ if __name__ == "__main__":
     os.makedirs(OUT_DIR, exist_ok=True)
 
     # Prepare data.
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    transform = v2.Compose(
-        [
-            v2.Grayscale(),
-            v2.PILToTensor(),
-            v2.ToDtype(torch.float32, scale=True),
-        ]
-    )
-    orig_data = ImageFolder(f"{DATA_DIR}/img", transform)
+    transforms = [
+        v2.Grayscale(),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float32, scale=True),
+    ]
+    orig_data = ImageFolder(f"{DATA_DIR}/img", v2.Compose(transforms))
     train_data, test_data = random_split(orig_data, [0.9, 0.1])
-    train_loader = DataLoader(train_data, batch_size=128, num_workers=8, pin_memory=True)
-    test_loader = DataLoader(test_data, batch_size=128, num_workers=8, pin_memory=True)
+    train_loader = DataLoader(
+        train_data,
+        batch_size=128,
+        num_workers=8,
+        pin_memory=True,
+    )
+    test_loader = DataLoader(
+        test_data,
+        batch_size=128,
+        num_workers=8,
+        pin_memory=True,
+    )
 
     # Define model.
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = nn.Sequential(
         nn.Conv2d(1, 16, 5),
         nn.ReLU(),
@@ -97,13 +109,15 @@ if __name__ == "__main__":
     ).to(device)
     torchinfo.summary(model)
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.00025)
+    optimizer = optim.AdamW(model.parameters())
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2)
 
     # Train model.
-    for epoch in range(10):
+    for epoch in range(20):
         print(f"\n### Epoch {epoch}")
         train_loop(train_loader, model, loss_fn, optimizer, device)
-        test_loop(test_loader, model, loss_fn, device)
+        test_loss = test_loop(test_loader, model, loss_fn, device)
+        scheduler.step(test_loss)
         print("-------------------------------------")
 
     # Export ONNX model.
