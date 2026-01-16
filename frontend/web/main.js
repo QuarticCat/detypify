65b279bc0f53016c65e57e1e6d1c182cae043725
QuarticCat <QuarticCat@pm.me>
2026-01-16 23:48:53 +0800

    remove detypify-web entirely

--- frontend/web/main.js
+++
@@ -1,5 +0,0 @@
-import App from "./App.svelte";
-
-export default new App({
-    target: document.getElementById("app"),
-});
--- frontend/web/utils/Button.svelte
+++
@@ -1,7 +0,0 @@
-<button
-    type="button"
-    class="text-black hover:text-primary-700 dark:text-gray-400 dark:hover:text-white {$$props.class}"
-    on:click
->
-    <slot />
-</button>
