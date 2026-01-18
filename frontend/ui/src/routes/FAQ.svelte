b400a78c796f5b392541842859bb6652bf38bba5
QuarticCat <QuarticCat@pm.me>
2026-01-19 03:47:01 +0800

    re-design FAQ page

--- frontend/ui/src/routes/FAQ.svelte
+++ frontend/ui/src/routes/FAQ.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
-    import { A, Heading, P } from "flowbite-svelte";
+    import { inferSyms } from "detypify-service";
+    import { AccordionItem, Accordion, A, P, Li, List } from "flowbite-svelte";
+
+    const supportedSyms = inferSyms.flatMap((info) => info.names);
+    supportedSyms.sort();
 </script>
 
 <div class="ui-sub-container w-180">
-    <!-- TODO: List supported symbols. -->
-
-    <Heading tag="h4">Want to use it offline?</Heading>
-    <P>
-        Check the <A href="https://support.google.com/chrome/answer/9658361">guide</A>.
-    </P>
-
-    <Heading tag="h4">Like it?</Heading>
-    <P>
-        Star me on <A href="https://github.com/QuarticCat/detypify">GitHub</A>!
-    </P>
+    <Accordion>
+        <AccordionItem>
+            {#snippet header()}Supported symbols{/snippet}
+            <List>
+                {#each supportedSyms as name}
+                    <Li>{name}</Li>
+                {/each}
+            </List>
+        </AccordionItem>
+        <AccordionItem>
+            {#snippet header()}Use it offline{/snippet}
+            <P>
+                Check the <A href="https://support.google.com/chrome/answer/9658361">guide</A>.
+            </P>
+        </AccordionItem>
+        <AccordionItem>
+            {#snippet header()}Support us{/snippet}
+            <P>
+                Star this project on <A href="https://github.com/QuarticCat/detypify">GitHub</A>!
+            </P>
+        </AccordionItem>
+    </Accordion>
 </div>
