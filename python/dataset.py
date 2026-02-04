fded87a54d3f85210a8c48e3fd53a3ac83b649bf
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2026-02-04 19:22:21 +0800

    refactor dataset code

--- python/dataset.py
+++ python/dataset.py
@@ -1,12 +1,9 @@
 from os import process_cpu_count
-from typing import Literal
+from typing import override
 
-import torch
-from datasets import Array2D, Dataset, DatasetDict, fingerprint, load_dataset
+from datasets import load_dataset
 from lightning import LightningDataModule
-from proc_data import DATASET_REPO, rasterize_strokes
 from torch.utils.data import DataLoader
-from torchvision.transforms import v2
 
 
 class MathSymbolDataModule(LightningDataModule):
@@ -14,54 +11,28 @@ class MathSymbolDataModule(LightningDataModule):
         self,
         image_size: int,
         batch_size: int = 64,
-        num_workers: int = min(process_cpu_count(), 12),
+        num_workers: int = process_cpu_count(),
     ):
+        from proc_data import DATASET_REPO
+
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.image_size = image_size
+        self.dataset_repo = DATASET_REPO
 
-        base_transforms = [
-            v2.ToImage(),
-            v2.ToDtype(dtype=torch.float32, scale=True),
-        ]
-
-        augmentations = [
-            v2.RandomRotation(10),  # type: ignore[arg-type]
-            v2.RandomAffine(
-                degrees=0,  # type: ignore[arg-type]
-                translate=(0.1, 0.1),
-                shear=10,
-            ),
-        ]
-
-        # Compose them
-        self.train_transform = v2.Compose(base_transforms + augmentations)
-
-        # Eval: Same base + normalize, BUT NO ROTATION/SHIFT
-        self.eval_transform = v2.Compose(base_transforms)
-
+    @override
     def prepare_data(self):
-        load_dataset(DATASET_REPO)
+        load_dataset(self.dataset_repo)
 
+    @override
     def setup(self, stage: str | None = None):
-        dataset = load_dataset(DATASET_REPO)
+        from datasets import Array2D, DatasetDict
+        from proc_data import rasterize_strokes
 
-        def train_transform(batch):
-            images = torch.tensor(batch["image"], dtype=torch.uint8)
-            images = self.train_transform(images.unsqueeze(1))
-            batch["image"] = images
-            return batch
+        dataset = load_dataset(self.dataset_repo)
 
-        def eval_transform(batch):
-            images = torch.tensor(batch["image"], dtype=torch.uint8)
-            images = self.eval_transform(images.unsqueeze(1))
-            batch["image"] = images
-            return batch
-
-        def process_dataset(
-            ds_split: Dataset, transform_type: Literal["eval", "train"]
-        ) -> DatasetDict:
+        def process_dataset(ds_split: DatasetDict) -> DatasetDict:
             def _rasterize_strokes_batched(batch, image_size):
                 batch["image"] = [
                     rasterize_strokes(strokes, image_size)
@@ -75,7 +46,6 @@ class MathSymbolDataModule(LightningDataModule):
                     batched=True,
                     remove_columns="strokes",
                     num_proc=self.num_workers,
-                    new_fingerprint=fingerprint.Hasher.hash(self.image_size),
                     fn_kwargs={"image_size": self.image_size},
                 )
                 .cast_column(
@@ -83,18 +53,18 @@ class MathSymbolDataModule(LightningDataModule):
                     Array2D(shape=(self.image_size, self.image_size), dtype="uint8"),
                 )
                 .with_format("torch")
-                .with_transform(
-                    train_transform if transform_type == "train" else eval_transform
-                )
             )
 
+        dataset = process_dataset(dataset)
+
         if stage == "fit":
-            self.train_dataset = process_dataset(dataset["train"], "train")
-            self.val_dataset = process_dataset(dataset["val"], "eval")
+            self.train_dataset = dataset["train"]
+            self.val_dataset = dataset["val"]
 
         if stage == "test" or stage is None:
-            self.test_dataset = process_dataset(dataset["test"], "eval")
+            self.test_dataset = dataset["test"]
 
+    @override
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,  # type: ignore
@@ -105,6 +75,7 @@ class MathSymbolDataModule(LightningDataModule):
             persistent_workers=self.num_workers > 0,
         )
 
+    @override
     def val_dataloader(self):
         return DataLoader(
             self.val_dataset,  # type: ignore
@@ -115,6 +86,7 @@ class MathSymbolDataModule(LightningDataModule):
             persistent_workers=self.num_workers > 0,
         )
 
+    @override
     def test_dataloader(self):
         return DataLoader(
             self.test_dataset,  # type: ignore
@@ -124,3 +96,46 @@ class MathSymbolDataModule(LightningDataModule):
             pin_memory=True,
             persistent_workers=self.num_workers > 0,
         )
+
+    @override
+    def on_after_batch_transfer(self, batch, dataloader_idx):
+        # the data should be transferred to the right device automatically
+        from torch import Tensor
+        from torch import float32 as t_float32
+        from torch import uint8 as t_uint8
+        from torchvision.transforms import v2
+
+        eval_transform = v2.Compose(
+            [v2.ToImage(), v2.ToDtype(dtype=t_float32, scale=True)]
+        )
+        train_transform = v2.Compose(
+            [
+                v2.ToImage(),
+                # augmentations
+                v2.RandomRotation(10),  # type: ignore[arg-type]
+                v2.RandomAffine(
+                    degrees=0,  # type: ignore[arg-type]
+                    translate=(0.1, 0.1),
+                    shear=10,
+                ),
+                v2.ToDtype(dtype=t_float32, scale=True),
+            ]
+        )
+
+        def apply_transform(batch, transform: v2.Compose):
+            # make sure that the format is torch.Tensor
+            assert isinstance(batch["image"], Tensor)
+            images = batch["image"].to(dtype=t_uint8).unsqueeze(1)
+            images = transform(images)
+            batch["image"] = images
+            return batch
+
+        if self.trainer:
+            from lightning.pytorch.trainer.states import RunningStage
+
+            match self.trainer.state.stage:
+                case RunningStage.TRAINING:
+                    batch["image"] = apply_transform(batch["image"], train_transform)
+                case _:
+                    batch["image"] = apply_transform(batch["image"], eval_transform)
+        return batch
