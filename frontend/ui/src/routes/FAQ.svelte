152680efa8dde76f7bd8f17c4f16b393b9be325d
QuarticCat <QuarticCat@pm.me>
2026-02-10 14:12:29 +0800

    update FAQ

--- frontend/ui/src/routes/FAQ.svelte
+++ frontend/ui/src/routes/FAQ.svelte
@@ -9,7 +9,14 @@
 <div class="ui-sub-container w-180">
     <Accordion>
         <AccordionItem>
-            {#snippet header()}Supported symbols{/snippet}
+            {#snippet header()}Can't find your symbol?{/snippet}
+            <P>
+                Supported symbols are listed in section below. To recognize characters out of this scope, consider
+                giving <A href="https://shapecatcher.com/">shapecatcher</A> a try!
+            </P>
+        </AccordionItem>
+        <AccordionItem>
+            {#snippet header()}Supported symbols?{/snippet}
             <List>
                 {#each supportedSyms as name}
                     <Li>{name}</Li>
@@ -17,7 +24,16 @@
             </List>
         </AccordionItem>
         <AccordionItem>
-            {#snippet header()}Use it offline{/snippet}
+            {#snippet header()}Results are inaccurate?{/snippet}
+            <P>
+                Some privacy settings (like <A href="https://brave.com/shields/">Brave Shields</A>) have canvas
+                anti-fingerprinting, which might confuse the classifier. Make sure they are turned off for this site.
+                Don't worry! We don't collect any privacy data. This project is fully open-source and works locally on
+                your machine.
+            </P>
+        </AccordionItem>
+        <AccordionItem>
+            {#snippet header()}Use it offline?{/snippet}
             <P>
                 Check the <A href="https://support.google.com/chrome/answer/9658361">guide</A>.
             </P>
